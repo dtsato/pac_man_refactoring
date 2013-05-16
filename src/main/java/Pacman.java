eb587512d3c4f5bf153304d56aea655fa6aa3907
Danilo Sato <dtsato@gmail.com>
2013-05-16 01:00:07 -0700

    Renaming board

--- src/main/java/Pacman.java
+++ src/main/java/Pacman.java
@@ -13,33 +13,33 @@ public class Pacman extends JApplet implements MouseListener, KeyListener {
     private long timer = -1;
 
     /* Create a new board */
-    private Board b;
+    private Board board;
 
     /* This timer is used to do request new frames be drawn*/
     private Timer frameTimer;
 
     /* This constructor creates the entire game essentially */
     public Pacman() {
-        b = new Board();
-        b.requestFocus();
+        board = new Board();
+        board.requestFocus();
 
         /* Create and set up window frame*/
         JFrame f = new JFrame();
         f.setSize(420, 460);
 
         /* Add the board to the frame */
-        f.add(b, BorderLayout.CENTER);
+        f.add(board, BorderLayout.CENTER);
 
         /*Set listeners for mouse actions and button clicks*/
-        b.addMouseListener(this);
-        b.addKeyListener(this);
+        board.addMouseListener(this);
+        board.addKeyListener(this);
 
         /* Make frame visible, disable resizing */
         f.setVisible(true);
         f.setResizable(false);
 
         /* Set the gameFrame flag to 1 because this is a new game */
-        b.gameFrame = 1;
+        board.gameFrame = 1;
 
         /* Manually call the first frameStep to initialize the game. */
         stepFrame(true);
@@ -54,76 +54,76 @@ public class Pacman extends JApplet implements MouseListener, KeyListener {
         /* Start the timer */
         frameTimer.start();
 
-        b.requestFocus();
+        board.requestFocus();
     }
 
     /* This repaint function repaints only the parts of the screen that may have changed.
        Namely the area around every player ghost and the menu bars
     */
     public void repaint() {
-        if (b.player.teleport) {
-            b.repaint(b.player.lastX - 20, b.player.lastY - 20, 80, 80);
-            b.player.teleport = false;
+        if (board.player.teleport) {
+            board.repaint(board.player.lastX - 20, board.player.lastY - 20, 80, 80);
+            board.player.teleport = false;
         }
-        b.repaint(0, 0, 600, 20);
-        b.repaint(0, 420, 600, 40);
-        b.repaint(b.player.x - 20, b.player.y - 20, 80, 80);
-        b.repaint(b.ghost1.x - 20, b.ghost1.y - 20, 80, 80);
-        b.repaint(b.ghost2.x - 20, b.ghost2.y - 20, 80, 80);
-        b.repaint(b.ghost3.x - 20, b.ghost3.y - 20, 80, 80);
-        b.repaint(b.ghost4.x - 20, b.ghost4.y - 20, 80, 80);
+        board.repaint(0, 0, 600, 20);
+        board.repaint(0, 420, 600, 40);
+        board.repaint(board.player.x - 20, board.player.y - 20, 80, 80);
+        board.repaint(board.ghost1.x - 20, board.ghost1.y - 20, 80, 80);
+        board.repaint(board.ghost2.x - 20, board.ghost2.y - 20, 80, 80);
+        board.repaint(board.ghost3.x - 20, board.ghost3.y - 20, 80, 80);
+        board.repaint(board.ghost4.x - 20, board.ghost4.y - 20, 80, 80);
     }
 
     /* Steps the screen forward one frame */
     public void stepFrame(boolean New) {
         /* If we aren't on a special screen than the timers can be set to -1 to disable them */
-        if (!b.titleScreen && !b.winScreen && !b.overScreen) {
+        if (!board.titleScreen && !board.winScreen && !board.overScreen) {
             timer = -1;
             titleTimer = -1;
         }
 
         /* If we are playing the dying animation, keep advancing frames until the animation is complete */
-        if (b.dying > 0) {
-            b.repaint();
+        if (board.dying > 0) {
+            board.repaint();
             return;
         }
 
         /* gameFrame can either be specified by the gameFrame parameter in stepFrame function call or by the state
-  of b.gameFrame.  Update gameFrame accordingly */
-        New = New || (b.gameFrame != 0);
+  of board.gameFrame.  Update gameFrame accordingly */
+        New = New || (board.gameFrame != 0);
 
         /* If this is the title screen, make sure to only stay on the title screen for 5 seconds.
 If after 5 seconds the user hasn't started a game, start up demo mode */
-        if (b.titleScreen) {
+        if (board.titleScreen) {
             if (titleTimer == -1) {
                 titleTimer = System.currentTimeMillis();
             }
 
             long currTime = System.currentTimeMillis();
             if (currTime - titleTimer >= 5000) {
-                b.titleScreen = false;
-                b.demo = true;
+                board.titleScreen = false;
+                board.demo = true;
                 titleTimer = -1;
             }
-            b.repaint();
+            board.repaint();
             return;
         }
 
         /* If this is the win screen or game over screen, make sure to only stay on the screen for 5 seconds.
 If after 5 seconds the user hasn't pressed a key, go to title screen */
-        else if (b.winScreen || b.overScreen) {
+        else if (board.winScreen || board.overScreen) {
             if (timer == -1) {
                 timer = System.currentTimeMillis();
             }
 
             long currTime = System.currentTimeMillis();
             if (currTime - timer >= 5000) {
-                b.winScreen = false;
-                b.overScreen = false;
-                b.titleScreen = true;
+                board.winScreen = false;
+                board.overScreen = false;
+                board.titleScreen = true;
                 timer = -1;
             }
-            b.repaint();
+            board.repaint();
             return;
         }
 
@@ -132,55 +132,55 @@ If after 5 seconds the user hasn't pressed a key, go to title screen */
         if (!New) {
             /* The pacman player has two functions, demoMove if we're in demo mode and move if we're in
    user playable mode.  Call the appropriate one here */
-            if (b.demo) {
-                b.player.demoMove();
+            if (board.demo) {
+                board.player.demoMove();
             } else {
-                b.player.move();
+                board.player.move();
             }
 
             /* Also move the ghosts, and update the pellet states */
-            b.ghost1.move();
-            b.ghost2.move();
-            b.ghost3.move();
-            b.ghost4.move();
-            b.player.updatePellet();
-            b.ghost1.updatePellet();
-            b.ghost2.updatePellet();
-            b.ghost3.updatePellet();
-            b.ghost4.updatePellet();
+            board.ghost1.move();
+            board.ghost2.move();
+            board.ghost3.move();
+            board.ghost4.move();
+            board.player.updatePellet();
+            board.ghost1.updatePellet();
+            board.ghost2.updatePellet();
+            board.ghost3.updatePellet();
+            board.ghost4.updatePellet();
         }
 
         /* We either have a new game or the user has died, either way we have to reset the board */
-        if (b.stopped || New) {
+        if (board.stopped || New) {
             /*Temporarily stop advancing frames */
             frameTimer.stop();
 
             /* If user is dying ... */
-            while (b.dying > 0) {
+            while (board.dying > 0) {
                 /* Play dying animation. */
                 stepFrame(false);
             }
 
             /* Move all game elements back to starting positions and orientations */
-            b.player.currDirection = 'L';
-            b.player.direction = 'L';
-            b.player.desiredDirection = 'L';
-            b.player.x = 200;
-            b.player.y = 300;
-            b.ghost1.x = 180;
-            b.ghost1.y = 180;
-            b.ghost2.x = 200;
-            b.ghost2.y = 180;
-            b.ghost3.x = 220;
-            b.ghost3.y = 180;
-            b.ghost4.x = 220;
-            b.ghost4.y = 180;
+            board.player.currDirection = 'L';
+            board.player.direction = 'L';
+            board.player.desiredDirection = 'L';
+            board.player.x = 200;
+            board.player.y = 300;
+            board.ghost1.x = 180;
+            board.ghost1.y = 180;
+            board.ghost2.x = 200;
+            board.ghost2.y = 180;
+            board.ghost3.x = 220;
+            board.ghost3.y = 180;
+            board.ghost4.x = 220;
+            board.ghost4.y = 180;
 
             /* Advance a frame to display main state*/
-            b.repaint(0, 0, 600, 600);
+            board.repaint(0, 0, 600, 600);
 
             /*Start advancing frames once again*/
-            b.stopped = false;
+            board.stopped = false;
             frameTimer.start();
         }
         /* Otherwise we're in a normal state, advance one frame*/
@@ -192,39 +192,39 @@ If after 5 seconds the user hasn't pressed a key, go to title screen */
     /* Handles user key presses*/
     public void keyPressed(KeyEvent e) {
         /* Pressing a key in the title screen starts a game */
-        if (b.titleScreen) {
-            b.titleScreen = false;
+        if (board.titleScreen) {
+            board.titleScreen = false;
             return;
         }
         /* Pressing a key in the win screen or game over screen goes to the title screen */
-        else if (b.winScreen || b.overScreen) {
-            b.titleScreen = true;
-            b.winScreen = false;
-            b.overScreen = false;
+        else if (board.winScreen || board.overScreen) {
+            board.titleScreen = true;
+            board.winScreen = false;
+            board.overScreen = false;
             return;
         }
         /* Pressing a key during a demo kills the demo mode and starts a new game */
-        else if (b.demo) {
-            b.demo = false;
+        else if (board.demo) {
+            board.demo = false;
             /* Stop any pacman eating sounds */
-            b.sounds.nomNomStop();
-            b.gameFrame = 1;
+            board.sounds.nomNomStop();
+            board.gameFrame = 1;
             return;
         }
 
         /* Otherwise, key presses control the player! */
         switch (e.getKeyCode()) {
             case KeyEvent.VK_LEFT:
-                b.player.desiredDirection = 'L';
+                board.player.desiredDirection = 'L';
                 break;
             case KeyEvent.VK_RIGHT:
-                b.player.desiredDirection = 'R';
+                board.player.desiredDirection = 'R';
                 break;
             case KeyEvent.VK_UP:
-                b.player.desiredDirection = 'U';
+                board.player.desiredDirection = 'U';
                 break;
             case KeyEvent.VK_DOWN:
-                b.player.desiredDirection = 'D';
+                board.player.desiredDirection = 'D';
                 break;
         }
 
@@ -233,7 +233,7 @@ If after 5 seconds the user hasn't pressed a key, go to title screen */
 
     /* This function detects user clicks on the menu items on the bottom of the screen */
     public void mousePressed(MouseEvent e) {
-        if (b.titleScreen || b.winScreen || b.overScreen) {
+        if (board.titleScreen || board.winScreen || board.overScreen) {
             /* If we aren't in the game where a menu is showing, ignore clicks */
             return;
         }
@@ -244,10 +244,10 @@ If after 5 seconds the user hasn't pressed a key, go to title screen */
         if (400 <= y && y <= 460) {
             if (100 <= x && x <= 150) {
                 /* New game has been clicked */
-                b.gameFrame = 1;
+                board.gameFrame = 1;
             } else if (180 <= x && x <= 300) {
                 /* Clear high scores has been clicked */
-                b.clearHighScores();
+                board.clearHighScores();
             } else if (350 <= x && x <= 420) {
                 /* Exit has been clicked */
                 System.exit(0);
